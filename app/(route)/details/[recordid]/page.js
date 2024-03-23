78855ac9d977be09baefa7e4c59da760f64eb186
MbusyaNgesa <theengesa@gmail.com>
2024-03-23 12:24:44 +0300

    Car About

--- app/(route)/details/[recordid]/page.js
+++ app/(route)/details/[recordid]/page.js
@@ -21,7 +21,7 @@ function Details({params}) {
     <div className='p-5 md:px-20'>
       <h2 className='font-bold text-[22px]'>Details</h2>\
 
-      <div className='grid grid-cols-1 md:grid-cols-4' >
+      <div className='grid grid-cols-1 lg:grid-cols-4' >
         {/*Car Details*/}
           <div className='col-span-3'>
          {car&& <CarDetail car={car} />}
