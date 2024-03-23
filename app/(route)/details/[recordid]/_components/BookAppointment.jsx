26550f131b2972ee2700c162e0c2624cd3d193a6
MbusyaNgesa <theengesa@gmail.com>
2024-03-23 19:07:37 +0300

    Select Time SLot

--- app/(route)/details/[recordid]/_components/BookAppointment.jsx
+++ app/(route)/details/[recordid]/_components/BookAppointment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -9,12 +9,40 @@ import {
   } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { Calendar } from "@/components/ui/calendar"
-import { CalendarDays } from 'lucide-react'
+import { CalendarDays, Clock } from 'lucide-react'
 
   
 function BookAppointment() {
 
-    const [date, setDate] = useState(new Date)
+    const [date, setDate] = useState(new Date());
+    const [timeSlot, setTimeSlot] = useState([]);
+    const [selectedTimeslot, setSelectedTimeSlot]=useState();
+
+    useEffect(()=>{
+        getTime();
+    },[])
+
+    const getTime=() => {
+        const timeList = [];
+        for (let i=10; i<= 12; i++ ){
+            timeList.push({
+                time: i + ':00 AM'
+            })
+            timeList.push({
+                time: i + ':30 AM'
+            })
+        }
+        for (let i=1; i<= 6; i++ ){
+            timeList.push({
+                time: i + ':00 PM'
+            })
+            timeList.push({
+                time: i + ':30 PM'
+            })
+        }
+
+        setTimeSlot(timeList)
+    };
 
   return (
     <Dialog>
@@ -31,13 +59,13 @@ function BookAppointment() {
             <div className='grid grid-cols-1 md:grid-cols-2 mt-5'>
                    {/*Calendar */}
                    <div className='flex flex-col items-baseline'>
-                    <h2 className='flex-gap-2 items-center'>
+                    <h2 className='flex gap-2 items-center'>
                         <CalendarDays className='text-primary h-5 w-5' />
                         Select Date
                     </h2>
                    <Calendar
                         mode="single"
-                        selected={date}
+                         selected={date}
                         onSelect={setDate}
                         className="rounded-md border"
                     />
@@ -45,7 +73,23 @@ function BookAppointment() {
 
                     {/*Time Slot */}
 
-                    <div>
+                    <div className='mt-3 md:mt-0'>
+                        <h2 className='flex gap-2 items-center mb-3'>
+                            <Clock className='text-primary h-5 w-5'/>
+                            Select Time Slot
+                        </h2>
+                        <div className='grid grid-cols-3 gap-2 border 
+                        rounded-lg p-5'>
+                            {timeSlot?.map((item, index)=>(
+                                <h2
+                                onClick={()=>setSelectedTimeSlot(item.time)}
+                                className={`p-2 border cursor-pointer rounded-full
+                                text-center hover:bg-primary
+                                hover:text-white 
+                                ${item.time==selectedTimeslot&&'bg-primary text-white '}`}                              
+                                >{item.time}</h2>
+                            ))}
+                        </div>    
 
                     </div>
             </div>
@@ -55,7 +99,7 @@ function BookAppointment() {
   </DialogContent>
 </Dialog>
 
-  )
+  );
 }
 
 export default BookAppointment
